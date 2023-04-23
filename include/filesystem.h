4075b79d451779686e4ea13da71d3dd3785709ef
Leechy-Litchi <61399327+Leechy-Litchi@users.noreply.github.com>
2023-04-23 14:48:32 +0800

    Update filesystem.h

--- include/filesystem.h
+++ include/filesystem.h
@@ -1,43 +1,46 @@
 #ifndef FILESYSTEM_H
 #define FILESYSTEM_H
 
-#include <iostream>  
+#include "Node.h"
+#include <ctime>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
-#include <ctime>
 #include <vector>
-#include "Node.h"
 
 using namespace std;
 
 class FileSystem {
-public:
-    Node* root;          
-    Node* curNode;        
-    int curBlock;         
-    FileSystem();
+protected:
+  int curBlock;
+  Node *root;
+  Node *curNode;
+  void saveToFileRecursion(Node *node, ofstream &ofs);
+  void loadFromFileRecursion(ifstream &ifs, Node *curNode);
+  Node *findDir(string name);
+  Node *findDirRecursion(Node *node, string name);
+  Node *findFile(string name);
+  Node *findFileRecursion(Node *node, string name);
 
-    void showPath();
-    void showItems(Node* curNode);
-    void cdParent();
-    void cdChild(string name);
-    Node* newFile(string name);
-    void deleteFile(string name);
-    void newDir(string name); 
-    void deleteDir(string name);
-    void saveToFile(string path = "fs.dat");
-    void saveToFileRecursive(Node* node, ofstream& ofs);
-    void loadFromFile(string path = "fs.dat");
-    void loadFromFileRecursive(ifstream& ifs, Node* curNode);
-    Node* findDir(string name); 
-    void findDirRecursive(Node* node, string name, Node*& res);
-    int getDirSize(Node* node);
-    Node* findFile(string name);
-    void rename(string filename1, string filename2);
-    Node* getParentDir(Node* curNode);
-    Node* findFileRecursive(Node* node, string name);
-    void copyFile(string src, string dest);
+public:
+  Node *getCurNode();
+  Node *getRoot();
+  void cdChild(string name);
+  void cdParent();
+  void newDir(string name);
+  Node *newFile(string name);
+  void deleteFile(string name);
+  void deleteDir(string name);
+  void showItems(Node *curNode);
+  void showPath();
+  void saveToFile(string path = "fs.dat");
+  void loadFromFile(string path = "fs.dat");
+  int getDirSize(Node *node);
+  void rename(string filename1, string filename2);
+  void copyFile(string src, string dest);
+
+  FileSystem();
 };
 
 #endif
